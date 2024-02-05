ee36abf94fc741b13614124a3f780594d0563e20
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-05 23:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+   document.addEventListener('DOMContentLoaded', function () {
+      // Get the body element
+      var body = document.body;
+
+      // Create a new paragraph element
+      var paragraph = document.createElement('p');
+
+      // Set the text content of the paragraph
+      paragraph.textContent = "DOM load success";
+
+      // Append the paragraph to the body
+      body.appendChild(paragraph);
+    });
